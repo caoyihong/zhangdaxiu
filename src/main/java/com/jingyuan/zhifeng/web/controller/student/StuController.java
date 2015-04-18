4ac0111750173d0c902a1117b7f7cee9cb5552df
cloudLiu <545286400@q.com>
2015-04-18 14:41:10 +0800

    aa

--- src/main/java/com/jingyuan/zhifeng/web/controller/student/StuController.java
+++ src/main/java/com/jingyuan/zhifeng/web/controller/student/StuController.java
@@ -1,5 +1,9 @@
 package com.jingyuan.zhifeng.web.controller.student;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("stu")
 public class StuController {
+	
+	/**
+	 * 学生管理主界面
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String logout(HttpServletRequest request) {
+		Subject subject = SecurityUtils.getSubject();
+		subject.logout();
 
+		return "redirect:/";
+	}
 }
